336ff6424e9cf179ef535a6d8203b048b04dd50c
brouzie <sokrates.kjoelner@gmail.com>
2025-09-09 19:39:02 +0200

    completed themeswitcher with matugen, magick and pastel

--- default/hypr/scripts/wf-recorder/wf-rec-start.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-# generate a timestamped filename
-outfile="$HOME/Videos/recordings/$(date '+%Y-%m-%d_%H-%M-%S').mp4"
-# start recording in background
-wf-recorder --audio --framerate 60 -f "$outfile" &
-# record the PID so we can stop it
-echo $! > "$HOME/.cache/wf-recorder.pid"
-# optional notification
-notify-send "Screen recording started" "$outfile"
