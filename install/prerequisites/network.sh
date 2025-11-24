1c29fe76103a3496f85798b2df1659816faa0fb7
brouzie <sokrates.kjoelner@gmail.com>
2025-11-24 02:44:05 +0100

    NetworkManager enabled

--- install/prerequisites/network.sh
+++ install/prerequisites/network.sh
@@ -1,8 +1,33 @@
-sudo pacman -S --needed --noconfirm networkmanager wireless-regdb iwd
+# Version 1 wpa_supplicant:
+sudo pacman -S --needed --noconfirm \
+  networkmanager \
+  wireless-regdb
 
-# Ensure iwd service will be started
-sudo systemctl enable iwd.service
+sudo systemctl enable --now NetworkManager.service
 
-# Prevent systemd-networkd-wait-online timeout on boot
+# Safety: make sure nothing is fighting NM
+sudo systemctl disable --now systemd-networkd.service
 sudo systemctl disable systemd-networkd-wait-online.service
 sudo systemctl mask systemd-networkd-wait-online.service
+
+# Version 2 with iwd backend:
+
+# sudo pacman -S --needed --noconfirm \
+#   networkmanager \
+#   iwd \
+#   wireless-regdb
+
+
+# sudo mkdir -p /etc/NetworkManager/conf.d
+# sudo tee /etc/NetworkManager/conf.d/wifi_backend.conf >/dev/null <<EOF
+# [device]
+# wifi.backend=iwd
+# EOF
+
+# sudo systemctl enable --now iwd.service
+# sudo systemctl enable --now NetworkManager.service
+
+# sudo systemctl disable --now wpa_supplicant.service
+# sudo systemctl disable --now systemd-networkd.service
+# sudo systemctl disable systemd-networkd-wait-online.service
+# sudo systemctl mask systemd-networkd-wait-online.service
